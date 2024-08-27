4e1cfcb7327f3517dd269982848e47270c2dc65e
jellask23 <jellasaikiransatya@gmail.com>
2024-08-28 01:04:56 +0530

    feat: modified carousel animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "@/components/organisms/Navbar";
 import NewArrivals from "@/components/organisms/NewArrivals";
 import Tabs from "@/components/organisms/Tabs";
 import Carousel from "@/components/organisms/Test/Index";
-import Test from "@/components/organisms/Test/Index";
 import { Button } from "@nextui-org/react";
 
 const App = () => {
@@ -14,43 +13,87 @@ const App = () => {
       <Carousel
         // height="50rem"
         elements={[
-          <div className="carousel-content 1 flex">
-            <div className="w-full md:w-1/2">
-              <img
-                src={
-                  "https://png.pngtree.com/thumb_back/fh260/background/20231005/pngtree-smoke-engulfed-vape-mod-in-stunning-3d-render-image_13568825.png"
-                }
-                alt=""
-              />
-            </div>
-            <div className="w-full md:w-1/2">
-              <p className="text-5xl font-bold my-3 pt-16">
-                High Quality Smoking Accessories
-              </p>
-              <p className="text-base my-3">
-                Discover our exclusive collection of premium smoking gear.
-              </p>
-              <div className="flex gap-4 my-3">
-                <Button color="primary">Shop now</Button>
-                <Button color="primary">Learn More</Button>
+          <>
+            <div className="">
+              <div className="carousel-content 1 flex">
+                <div className="w-full md:w-1/2">
+                  <img
+                    src={
+                      "https://png.pngtree.com/thumb_back/fh260/background/20231005/pngtree-smoke-engulfed-vape-mod-in-stunning-3d-render-image_13568825.png"
+                    }
+                    alt=""
+                  />
+                </div>
+                <div className="w-full md:w-1/2">
+                  <p className="text-5xl font-bold my-3 pt-16">
+                    High Quality Smoking Accessories
+                  </p>
+                  <p className="text-base my-3">
+                    Discover our exclusive collection of premium smoking gear.
+                  </p>
+                  <div className="flex gap-4 my-3">
+                    <Button color="primary">Shop now</Button>
+                    <Button color="primary">Learn More</Button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>,
-          <img
-            src={
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfbAimTkoJWgje7eQPjbe8G7jUaGsNjdCUmw&s"
-            }
-            alt=""
-          />,
-          <div>
-            <Button color="primary">Shop now</Button>
-            <Button color="primary">Learn More</Button>
-          </div>,
-          <div>
-            <Button color="primary">Shop now</Button>
-          </div>
+          </>,
+          <>
+            <div className="">
+              <div className="carousel-content 1 flex">
+                <div className="w-full md:w-1/2">
+                  <img
+                    src={
+                      "https://png.pngtree.com/thumb_back/fh260/background/20231005/pngtree-smoke-engulfed-vape-mod-in-stunning-3d-render-image_13568825.png"
+                    }
+                    alt=""
+                  />
+                </div>
+                <div className="w-full md:w-1/2">
+                  <p className="text-5xl font-bold my-3 pt-16">
+                    Low Quality Smoking Accessories
+                  </p>
+                  <p className="text-base my-3">
+                    Discover our exclusive collection of premium smoking gear.
+                  </p>
+                  <div className="flex gap-4 my-3">
+                    <Button color="primary">Shop now</Button>
+                    <Button color="primary">Learn More</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>,
+          <>
+            <div className="">
+              <div className="carousel-content 1 flex">
+                <div className="w-full md:w-1/2">
+                  <img
+                    src={
+                      "https://png.pngtree.com/thumb_back/fh260/background/20231005/pngtree-smoke-engulfed-vape-mod-in-stunning-3d-render-image_13568825.png"
+                    }
+                    alt=""
+                  />
+                </div>
+                <div className="w-full md:w-1/2">
+                  <p className="text-5xl font-bold my-3 pt-16">
+                    Quality Smoking Accessories
+                  </p>
+                  <p className="text-base my-3">
+                    Discover our exclusive collection of premium smoking gear.
+                  </p>
+                  <div className="flex gap-4 my-3">
+                    <Button color="primary">Shop now</Button>
+                    <Button color="primary">Learn More</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>,
         ]}
-      />      <InitialCategories />
+      />
+      <InitialCategories />
       <NewArrivals />
       <Tabs />
     </>
