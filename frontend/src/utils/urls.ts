b1e0cb7ae28cd5cea00e2cca2075154a51d99158
jellask23 <jellasaikiransatya@gmail.com>
2024-08-28 23:30:11 +0530

    feat: added mock server

--- frontend/src/utils/urls.ts
+++ frontend/src/utils/urls.ts
@@ -2,3 +2,10 @@
 export const HOME_PAGE_ROUTE = "/home"
 export const SHOP_PAGE_ROUTE = "/shop"
 export const PRODUCTS_PAGE_ROUTE = "/products"
+
+// API Endpoints
+export const BASE_URL = "http://localhost:1234"
+export const USERS_URL = `${BASE_URL}/users`
+export const USER_BY_ID_URL = (id: string) => `${BASE_URL}/users/${id}`
+export const PRODUCTS_URL = `${BASE_URL}/products`
+export const PRODUCT_BY_ID_URL = (id: string) => `${BASE_URL}/products/${id}`
