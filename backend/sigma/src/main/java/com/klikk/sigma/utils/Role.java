4841a5a0851c745c5ac69f6f395ee806c7c7cc2b
DharaPranavKaushik <dharapranavkaushik@klikktech.com>
2024-10-01 00:20:54 -0500

    Removed the errors and made code changes based on the review

--- backend/sigma/src/main/java/com/klikk/sigma/utils/Role.java
+++ backend/sigma/src/main/java/com/klikk/sigma/utils/Role.java
@@ -1,5 +1,6 @@
 package com.klikk.sigma.utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
