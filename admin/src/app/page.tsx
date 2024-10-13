fc9f0b8b495edf7f2877f87882f9aad1fa8455d4
jellask23 <jellasaikiransatya@gmail.com>
2024-10-13 17:13:40 +0530

    feat: configured nextui theme

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,18 +1,5 @@
-import BreadcrumbSkeleton from "@/components/molecules/BreadCrumbSkeleton";
-import CardView from "@/components/molecules/Cardview";
-import SideNav from "@/components/organisms/SideNav";
+const Home = () => {
+  return <></>;
+};
 
-export default function Home() {
-  return (
-    // <div className="flex w-full">
-    //   <div className="w-1/6">
-    //     <SideNav />
-    //   </div>      
-    //   <div className="w-5/6 p-3">
-    //     <BreadcrumbSkeleton />
-    //     <CardView />
-    //   </div>
-    // </div>
-    <div className=""></div>
-  );
-}
+export default Home;
