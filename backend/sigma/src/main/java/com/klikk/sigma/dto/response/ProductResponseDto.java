2a1f7590d6f317c8dbaba74d0a0c6250a97bc9b9
DharaPranavKaushik <dharapranavkaushik@klikktech.com>
2024-10-23 01:47:20 -0500

    Update the product response dto to include variations

--- backend/sigma/src/main/java/com/klikk/sigma/dto/response/ProductResponseDto.java
+++ backend/sigma/src/main/java/com/klikk/sigma/dto/response/ProductResponseDto.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -19,4 +21,6 @@ public class ProductResponseDto {
     private Attachment displayImage;
 
     private String sku;
+
+    private List<VariationResponseDto> variations;
 }
