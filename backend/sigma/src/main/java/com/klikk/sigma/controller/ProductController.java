78f3b95d3758bd77d40b6e1b2824058a06b820e5
jellask23 <jellasaikiransatya@gmail.com>
2024-10-30 00:23:04 +0530

    feat: add multiple images for product backend

--- backend/sigma/src/main/java/com/klikk/sigma/controller/ProductController.java
+++ backend/sigma/src/main/java/com/klikk/sigma/controller/ProductController.java
@@ -1,20 +1,16 @@
 package com.klikk.sigma.controller;
 
 import com.klikk.sigma.dto.response.ProductResponseDto;
-import com.klikk.sigma.dto.response.ProductsResponse;
 import com.klikk.sigma.dto.response.VariationResponseDto;
-import com.klikk.sigma.entity.ProductRequestDto;
 import com.klikk.sigma.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -25,19 +21,6 @@ public class ProductController {
     @Autowired
     public ProductService productService;
 
-    @PostMapping()
-    @PreAuthorize("hasAnyAuthority('admin:write','admin:put')")
-    public ResponseEntity<String> addProduct(@RequestPart("product") ProductRequestDto productRequest,
-                                             @RequestPart(value = "displayImage",required = false) MultipartFile displayImage
-    ) {
-        try {
-            productService.saveProduct(productRequest, displayImage);
-            return ResponseEntity.ok("Product Added Successfully");
-        } catch (Exception exception) {
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-        }
-    }
-
     @GetMapping()
     public ResponseEntity<Page<ProductResponseDto>> getAllProducts(Pageable pageable){
         return ResponseEntity.ok().body(productService.getAllProducts(pageable));
@@ -46,12 +29,6 @@ public class ProductController {
     @GetMapping("/{details}")
     public ResponseEntity<ProductResponseDto> getProduct(@PathVariable String details){
         return ResponseEntity.ok().body((productService.getProduct(details)));
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasAnyAuthority('admin:read')")
-    public ResponseEntity<List<ProductsResponse>> getAllProductsForAdmin() {
-        return ResponseEntity.ok(productService.getAllProductsForAdmin());
     }
 
     @GetMapping({"/{details}/variations"})
