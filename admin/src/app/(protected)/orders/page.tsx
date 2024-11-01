fc970b0e763c9cd4d6d15bd9b6a4dc2494190311
jellask23 <jellasaikiransatya@gmail.com>
2024-11-01 22:58:02 +0530

    chore: remove unused files

--- admin/src/app/(protected)/orders/page.tsx
+++ admin/src/app/(protected)/orders/page.tsx
@@ -1,26 +1,11 @@
-import Breadcrumb from "@/components/molecules/BreadCrumb"
-import UserTable from "@/components/molecules/UserTable"
-import { User } from "@/components/molecules/UserTable/columns"
-
-async function getUsers(): Promise<User[]> {
-    const res = await fetch(
-        'https://64a6f5fc096b3f0fcc80e3fa.mockapi.io/api/users'
-    )
-    const data = await res.json()
-    return data
-}
+import Breadcrumb from "@/components/molecules/BreadCrumb";
 
 export default async function Orders() {
-    const users = await getUsers()
-
-    return (
-        <>
-            <Breadcrumb />
-            <section className='py-2'>
-                <div className='container'>
-                    <UserTable users={users} />
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <Breadcrumb />
+      {/* write logic for orders here */}
+      Orders coming soon..
+    </>
+  );
 }
