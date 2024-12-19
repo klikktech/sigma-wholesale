85f4c017f73ce32aab3ab8a17c6d23e0dd3aa502
klikktech <contact@klikktech.com>
2024-12-18 20:58:01 -0600

    Update logout URL

--- admin/src/utils/urls.ts
+++ admin/src/utils/urls.ts
@@ -1,7 +1,7 @@
 // Backend endpoints
 export const BASE_BACKEND_URL = process.env.BASE_BACKEND_URL;
 export const SIGNIN_ENDPOINT = `api/auth/admin/authenticate`;
-export const SIGNOUT_ENDPOINT = `api/auth/authenticate`;
+export const SIGNOUT_ENDPOINT = `api/auth/logout`;
 export const REFRESH_TOKEN_URL = `api/auth/refresh-token`;
 
 
